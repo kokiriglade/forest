6bbfbb70a350e4c1ed54e95ebaea98425359e5e3
kokiriglade <60290002+celerry@users.noreply.github.com>
2024-11-08 05:43:31 +0000

    Meta: we're a monorepo now

--- key/key_test.ts
+++ key/key_test.ts
@@ -4,6 +4,6 @@ import { Key } from "./key.ts";
 Deno.test("constructor overloading functions as expected", () => {
     assertEquals(
         Key.key("namespace", "value").asString(),
-        Key.key("namespace:value").asString()
+        Key.key("namespace:value").asString(),
     );
 });
